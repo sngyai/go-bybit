f2333ef86dda6f022a26f39771d569e0bd1675c3
YuDong Yang <yangyudong@yax.xyz>
2024-03-28 19:56:45 +0800

    Update Ticker Data

--- ws/wsv5/public_ticker.go
+++ ws/wsv5/public_ticker.go
@@ -72,15 +72,26 @@ type PublicTickersResponse struct {
 
 // PublicTickersData :
 type PublicTickersData struct {
-	Symbol        string `json:"symbol"`
-	LastPrice     string `json:"lastPrice"`
-	HighPrice24H  string `json:"highPrice24h"`
-	LowPrice24H   string `json:"lowPrice24h"`
-	PrevPrice24H  string `json:"prevPrice24h"`
-	Volume24H     string `json:"volume24h"`
-	Turnover24H   string `json:"turnover24h"`
-	Price24HPcnt  string `json:"price24hPcnt"`
-	UsdIndexPrice string `json:"usdIndexPrice"`
+	Symbol            string `json:"symbol"`
+	TickDirection     string `json:"tickDirection"`
+	Price24HPcnt      string `json:"price24hPcnt"`
+	LastPrice         string `json:"lastPrice"`
+	PrevPrice24H      string `json:"prevPrice24h"`
+	HighPrice24H      string `json:"highPrice24h"`
+	LowPrice24H       string `json:"lowPrice24h"`
+	PrevPrice1H       string `json:"prevPrice1h"`
+	MarkPrice         string `json:"markPrice"`
+	IndexPrice        string `json:"indexPrice"`
+	OpenInterest      string `json:"openInterest"`
+	OpenInterestValue string `json:"openInterestValue"`
+	Turnover24H       string `json:"turnover24h"`
+	Volume24H         string `json:"volume24h"`
+	NextFundingTime   string `json:"nextFundingTime"`
+	FundingRate       string `json:"fundingRate"`
+	Bid1Price         string `json:"bid1Price"`
+	Bid1Size          string `json:"bid1Size"`
+	Ask1Price         string `json:"ask1Price"`
+	Ask1Size          string `json:"ask1Size"`
 }
 
 // Key :
